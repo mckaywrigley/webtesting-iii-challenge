9ed646425204e38484dd2032ca5cce1cca520c8b
Mckay <mckaywrigley@gmail.com>
2019-05-15 06:47:01 -1000

    sets up files for testing

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,8 @@
-// Test away!
+import React from "react";
+import { render, fireEvent, cleanup } from "react-testing-library";
+import renderer from "react-test-renderer";
+import "react-testing-library/cleanup-after-each";
+
+import Display from "./Display";
+
+describe("<Display />", () => {});
